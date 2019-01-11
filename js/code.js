89a9f62a596cd02248796d05a6db7d1138ffee20
MathematicsCLtd <jesse@mathematicsconsulting.com>
2019-01-10 21:34:08 -0500

    Apply logic to toggle 'open' class

--- js/code.js
+++ js/code.js
@@ -1,6 +1,14 @@
-window.addEventListener("load", function(){
-var nodes = document.querySelectorAll(".lightbox");
-for(var i=0, i<nodes.length; i++){
-***STEP 4 HERE***
-}
+window.addEventListener("load", function() {
+        var nodes = document.querySelectorAll(".lightbox");
+        for(var i=0, i<nodes.length;
+            i++) {
+                nodes[i].addEventListener("click", function() {
+                    if(this.classList.contains("open")) {
+                        this.classList.remove("open")
+                    } else {
+                        this.classList.add("open");
+                        }
+                    });
+                })
+            }
 });
