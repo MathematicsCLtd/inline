392564f3922cd4bb1069e2153f572d37adad6839
MathematicsCLtd <jesse@mathematicsconsulting.com>
2019-01-12 20:56:51 -0500

    Initiates Lightbox class

--- js/code.js
+++ js/code.js
@@ -1,7 +1,6 @@
 window.addEventListener("load", function() {
         var nodes = document.querySelectorAll(".lightbox");
-        for(var i=0, i<nodes.length;
-            i++) {
+        for(var i=0, i<nodes.length; i++) {
                 nodes[i].addEventListener("click", function() {
                     if(this.classList.contains("open")) {
                         this.classList.remove("open")
